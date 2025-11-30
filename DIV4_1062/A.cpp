d14d2428c441d70318767cecaba2549a4e3e3b13
Zen <151759677+huyhduve@users.noreply.github.com>
2025-11-30 22:03:51 +0700

    DIV4_1062

---
+++ DIV4_1062/A.cpp
@@ -0,0 +1,43 @@
+//  ^------^
+// ( '(oo)' )
+// (  u  u  )
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+
+#define ff first
+#define sc second
+#define all(x) (x).begin(), (x).end()
+
+
+#define IN "A.IN"
+#define OUT "A.OUT"
+#define cerr if(0) cerr
+
+int const MOD = 1e9 + 7;
+
+void solve(){
+    int a, b, c, d; cin >> a >> b >> c >> d;
+    if(a == b && b == c && c == d){
+        cout << "YES\n";
+    }
+    else cout << "NO\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    if(fopen(IN, "r")){
+        freopen(IN, "r", stdin);
+        freopen(OUT, "w", stdout);
+        freopen("CERR.txt", "w", stderr);
+    }
+    int t = 1; cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
